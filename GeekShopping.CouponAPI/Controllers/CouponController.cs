6add329247381387cdf03ff2a46d8b793fdb4503
Magno Belloni <bellonidesouza6@gmail.com>
2022-02-06 12:58:19 -0300

    API de Cupom

---
+++ GeekShopping.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,28 @@
+﻿using GeekShopping.CouponAPI.Data.ValueObjects;
+using GeekShopping.CouponAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.CouponAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/coupon")]
+    public class CouponController : ControllerBase
+    {
+        private readonly ICouponRepository _repository;
+
+        public CouponController(ICouponRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{couponCode}")]
+        public async Task<ActionResult<CouponVO>> FindById(string couponCode)
+        {
+            var coupon = await _repository.GetCouponByCouponCode(couponCode);
+            if (coupon == null)
+                return NotFound();
+
+            return Ok(coupon);
+        }
+    }
+}
